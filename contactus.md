b3cc52ff6cb3f67fd9391c2963d5bbb1c4682e4a
Daniel Pett <dpett@britishmuseum.org>
2018-12-19 17:33:36 +0000

    Add contact form, DRY front page and coffins

---
+++ contactus.md
@@ -0,0 +1,9 @@
+---
+layout: news
+title: Contact us
+description: Contact us with queries or feedback
+permalink: /contactus
+---
+<div class="container mb-3">
+    <div id="feedback-form" class="contact"></div>
+</div>
---
+++ js/feedback.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('#feedback-form').ZammadForm({
+    messageTitle: 'Feedback Form',
+    messageSubmit: 'Submit',
+    messageThankYou: 'Thank you for your message (#%s)! We\'ll contact you as soon as possible.',
+    noCSS: true
+  });
+});
