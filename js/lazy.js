6db9bec24f740e15d7f13ef93a41d45c2c60d06b
Daniel Pett <dejp3@cam.ac.uk>
2019-03-13 08:53:58 +0000

    Added lazy load

---
+++ js/lazy.js
@@ -0,0 +1,9 @@
+$('.lazy').Lazy({
+        // your configuration goes here
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        visibleOnly: true,
+        onError: function(element) {
+            console.log('error loading ' + element.data('src'));
+        }
+    });
