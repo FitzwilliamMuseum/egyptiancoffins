8eb5122595c3cbb44278208713fb02446daec0b2
Daniel Pett <dejp3@cam.ac.uk>
2021-02-10 00:49:37 +0000

    Fix search layout

--- js/search.js
+++ js/search.js
@@ -1,75 +1,62 @@
-    // var documents = [];
-    let jsondata = "";
-    let apiUrl = "/data.json"
+let apiUrl = "/data.json"
 
-    async function getJson(url) {
-        let response = await fetch(url);
-        let data = await response.json()
-        return data;
+async function getJson(url) {
+    let response = await fetch(url);
+    let data = await response.json()
+    return data;
+}
+
+async function main() {
+    // getJson(apiUrl)
+    //     .then(data => console.log(data));
+    let documents = await getJson(apiUrl)
+
+    let idx = lunr(function () {
+      this.field('id')
+      this.field('title', { boost: 10 })
+      this.field('author')
+      this.field('content')
+      this.field('summary')
+      this.field('url')
+      Object.entries(documents).forEach(function (document) {
+        this.add( {
+          "id": document[0],
+          "title": document[1]['title'],
+          'author': document[1]['author'],
+          'content': document[1]['content'],
+          'summary': document[1]['summary']
+        })
+      }, this)
+    });
+    let searchParams = new URLSearchParams(window.location.search)
+    let param = searchParams.get('query')
+    var results = idx.search(param);
+
+    for (result of results) {
+      var doc = documents[result.ref];
+      $( "#search_results" ).append('<li><a href="'+doc.url+'">'+doc.title+'</a><br/>'+doc.summary+'</li>');
     }
+}
 
-    async function main() {
-        // getJson(apiUrl)
-        //     .then(data => console.log(data));
-        let documents = await getJson(apiUrl)
-        // console.log(documents);
-
-        let idx = lunr(function () {
-          this.field('id')
-          this.field('title', { boost: 10 })
-          this.field('author')
-          this.field('content')
-          this.field('summary')
-          this.field('url')
-          Object.entries(documents).forEach(function (document) {
-            console.log(document[0])
-            console.log(document[1]['title'])
-            // console.log(value)
-            this.add( {
-              "id": document[0],
-              "title": document[1]['title'],
-              'author': document[1]['author'],
-              'content': document[1]['content'],
-              'summary': document[1]['summary']
-            })
-          }, this)
-        });
-        console.log(idx)
-        let searchParams = new URLSearchParams(window.location.search)
-        console.log(searchParams.get('query'))
-        let param = searchParams.get('query')
-        var results = idx.search(param);
-        console.log(results)
-
-        for (result of results) {
-        console.log(result)
-              var doc = documents[result.ref]; // get document via the lunr id
-              $( "#search_results" ).append('<li><a href="'+doc.url+'">'+doc.title+'</a><br/>'+doc.summary+'</li>');
-            }
-        // console.log(param)
-
-
-    }
-
-    main();
+main();
 
 
 
 
-  // // watch input
-  // var $resultsContainer;
-  //
-  // $(function(){
-  //
-  //   // console.log(idx)
-  //   // console.log(idx.search(param))
-  //   var $resultsContainer = $('#search_results');
-  //     $resultsContainer.empty();
-  //     if(param.length < 2) return;
-  //     var results = idx.search(param);
-  //     // console.log(results)
-  //     for (result of results) {
-  //       var doc = documents[result.ref]; // get document via the lunr id
-  //       $resultsContainer.append('<li><a href="'+doc.url+'">'+doc.title+'</a></li>');
-  //     }
-  // })
+// // watch input
+// var $resultsContainer;
+//
+// $(function(){
+//
+//   // console.log(idx)
+//   // console.log(idx.search(param))
+//   var $resultsContainer = $('#search_results');
+//     $resultsContainer.empty();
+//     if(param.length < 2) return;
+//     var results = idx.search(param);
+//     // console.log(results)
+//     for (result of results) {
+//       var doc = documents[result.ref]; // get document via the lunr id
+//       $resultsContainer.append('<li><a href="'+doc.url+'">'+doc.title+'</a></li>');
+//     }
+// })
--- search.md
+++ search.md
@@ -2,6 +2,6 @@
 layout: search
 id: search
 permalink: /search
+title: Search results
 ---
-<h2>Search results</h2>
 <ul id="search_results" class="search_results"></ul>
